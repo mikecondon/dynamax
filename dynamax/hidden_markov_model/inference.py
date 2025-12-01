19330338a25510e88388686eef9a41648c05c895
colecitrenbaum <colecitrenbaum@gmail.com>
2025-11-30 18:30:13 -0800

    Fixed off by one indexing for nonstationary transitions, added test comparing hmm_two_filter_smoother and hmm_smoother for non-stationary kernel.

--- dynamax/hidden_markov_model/inference.py
+++ dynamax/hidden_markov_model/inference.py
@@ -189,7 +189,8 @@ def hmm_backward_filter(
         """Backward filtering step."""
         log_normalizer, backward_pred_probs = carry
 
-        A = get_trans_mat(transition_matrix, transition_fn, t)
+        # A = get_trans_mat(transition_matrix, transition_fn, t)
+        A = get_trans_mat(transition_matrix, transition_fn, t-1)
         ll = log_likelihoods[t]
 
         # Condition on emission at time t, being careful not to overflow.
